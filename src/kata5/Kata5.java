1777d9d661918ddcb797800d98f4e61f674b0d89
Rubén Santana Lorenzo <rubensanlor@gmail.com>
2021-11-11 10:00:19 +0000

    Kata5P1 version 3

--- src/kata5/Kata5.java
+++ src/kata5/Kata5.java
@@ -1,20 +1,41 @@
 package kata5;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.sql.ResultSet;
+import java.sql.PreparedStatement;
 
 public class Kata5 {
 
     private static void createNewTable(Connection conn) throws SQLException{
-        String sql = "CREATE TABLE IF NOT EXISTS email(\n"
-                        + "id integer primary key autoincrement,\n"
-                        + "direccion text NOT NULL);";
+        String sql = """
+                     CREATE TABLE IF NOT EXISTS email(
+                     id integer primary key autoincrement,
+                     direccion text NOT NULL);""";
         
         Statement stmt = conn.createStatement();
         stmt.execute(sql);
+        
+        try(BufferedReader br = new BufferedReader(new FileReader("email.txt")))
+        {
+            String email;
+            String insertSql = """
+                               INSERT INTO email(direccion) VALUES (?)
+                               """;
+            PreparedStatement pstmt = conn.prepareStatement(insertSql);
+            while((email = br.readLine()) != null){
+                pstmt.setString(1,email);
+                pstmt.executeUpdate();
+            }
+            
+        } catch (IOException e){
+            System.out.println(e.getMessage());
+        }
     }
     
     public static void main(String[] args) {
